48ababcabaa920ca9deb5619c7f9d382154d7f31
re-fuku <freoct20@gmail.com>
2026-04-28 10:32:23 +0900

    Circles UI調整

--- src/components/previews/Circles.tsx
+++ src/components/previews/Circles.tsx
@@ -8,7 +8,11 @@ type Props = {
 export default function Circles({size, colors}: Props) {
     return (
         <div
-            className="h-full w-full pl-2 pr-2 rounded-2xl flex items-center justify-center bg-white"
+            className="
+                h-full w-full pl-5 pr-5 rounded-2xl 
+                flex flex-wrap shrink-0 grow-0 items-center content-center justify-evenly
+                 bg-white overflow-hidden
+            "
             style={{
                 gap: `${size / 3}%`
             }}
