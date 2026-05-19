d16403eac903f33cea8ad7eca26f114c04c6e16f
re-fuku <freoct20@gmail.com>
2026-05-20 08:48:37 +0900

    UI調整

--- src/components/sidepanels/LayoutPanel.tsx
+++ src/components/sidepanels/LayoutPanel.tsx
@@ -9,15 +9,15 @@ type Props = {
 export default function LayoutPanel({selectedLayout, setSelectedLayout, layoutIcons, hoverStyle}: Props) {
 
     return (
-        <div className= "p-2.5 flex flex-col w-full h-full scrollbar-hide bg-cyan-700 rounded-tr-3xl overflow-auto">
+        <div className= "p-2.5 flex flex-col justify-center w-full h-full scrollbar-hide bg-cyan-700 rounded-tr-3xl overflow-x-hidden overflow-y-auto">
             {/* 3列のレイアウトのグリッド */}
             <div className="grid grid-cols-3 gap-5">
                 {layoutIcons.map((icon, index) => (
                     <button
                         key={index}
                         className={`
-                            flex items-start rounded-full aspect-square bg-item-bg w-20 h-20
-                            ${selectedLayout === icon ? 'border-4 border-sky-600 rounded-xl': ''}
+                            flex items-start aspect-square bg-item-bg w-20 h-20
+                            ${selectedLayout === icon ? 'border-4 border-sky-600 ': ''}
                             `
                         }
                         onClick={() => setSelectedLayout(icon)}
@@ -25,7 +25,7 @@ export default function LayoutPanel({selectedLayout, setSelectedLayout, layoutIc
                         <img
                             src={icon}
                             alt={`Layout ${index + 1}`}
-                            className={`object-contain ${hoverStyle}`}
+                            className={`object-contain rounded-2xl ${hoverStyle}`}
                         />
                     </button>
                 ))}
